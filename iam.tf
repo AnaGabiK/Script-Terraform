f80b65179f3c8708e3eb340f7bda5d98dcbb1408
Ana Gabrielle Kataoka <64393512+AnaGabiK@users.noreply.github.com>
2023-08-14 20:56:33 -0300

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,4 +1,3 @@
-#aqui vcs estao criando os grupos admin e dev
 resource "aws_iam_group" "admin_group" {
   name = "admin"
 }
@@ -7,7 +6,6 @@ resource "aws_iam_group" "dev_group" {
   name = "dev"
 }
 
-#aqui vcs estao adicionando os usuarios aos grupos que vcs criaram ali em cima
 variable "admin_users" {
   default = ["ana", "fer"]
 }
@@ -16,7 +14,6 @@ variable "dev_users" {
   default = ["user1", "user2", "user3"]
 }
 
-#aqui vcs estao criando de fato esses usuarios e grupos definidos ali em cima la na aws
 resource "aws_iam_user" "admin_users" {
   count = length(var.admin_users)
   name  = var.admin_users[count.index]
@@ -27,7 +24,6 @@ resource "aws_iam_user" "dev_users" {
   name  = var.dev_users[count.index]
 }
 
-#aqui vcs estao adicionando de fato esses usuarios aos grupos definidos ali em cima la na aws
 resource "aws_iam_group_membership" "admin_group_membership" {
   name  = "admin_group_membership"
   group = aws_iam_group.admin_group.name
